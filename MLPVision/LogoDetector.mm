fd47e287fe79c3c6465a18da6bb38c580f813727
Flora Jin <florajin14@gmail.com>
2014-02-24 21:39:31 -0500

    added negative training imgs

---
+++ MLPVision/LogoDetector.mm
@@ -0,0 +1,36 @@
+//
+//  LogoDetector.m
+//  MLPVision
+//
+//  Created by Flora on 2/20/14.
+//  Copyright (c) 2014 Flora. All rights reserved.
+//
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ml/ml.hpp>
+
+class LogoDetector
+{
+    
+}
+
+void LogoDetector::detectLogo(
+
+@implementation LogoDetector
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+
+- (void)detectLogo:(UIImage*)img {
+    // TODO : FILL
+}
+
+@end
